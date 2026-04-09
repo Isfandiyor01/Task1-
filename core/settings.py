23357cda56ab578dc5ef5d9c5a50c2e540e48399
Isfandiyor-Az <azimboevisfandier@gmail.com>
2026-04-09 11:55:03 +0500

    fix: added missing apps to settings

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'drf_spectacular',
+    'corsheaders', 
+
     'api',
 ]
 
@@ -62,6 +67,7 @@ SPECTACULAR_SETTINGS = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
