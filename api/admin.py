7bb689c8ee30cc4961561fb5fea6a77371d5e36b
Isfandiyor-Az <azimboevisfandier@gmail.com>
2026-04-09 15:20:49 +0500

    the project is done

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Book, Order, Reservation, Review
+
+
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Order)
+admin.site.register(Reservation)
+admin.site.register(Review)
 
 # Register your models here.
